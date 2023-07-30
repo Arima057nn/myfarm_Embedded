0a415a9a52c67819a6a5dc3e74d458d3a2e191da
VanDucKun <havanducdeptrai@gmail.com>
2023-07-30 11:02:08 +0700

    test add device

--- public/data/data.js
+++ public/data/data.js
@@ -1,7 +1,11 @@
-[
+const devices = [
   {
-    id: "mydeviceID",
-    position: "Nhà",
-    name: "Device 1",
-  },
+    "id": "mydeviceID",
+    "position": "Nhà",
+    "name": "Device 1",
+    "nguong_tuoi_tren": 70,
+    "nguong_tuoi_duoi": 50,
+    "nguong_suoi_tren": 30,
+    "nguong_tuoi_duoi": 20
+  }
 ];
